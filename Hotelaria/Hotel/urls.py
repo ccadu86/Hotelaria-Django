5591ed0bbe812d291cbce65333adce9816854eec
ccadu86 <ccadu86@gmail.com>
2025-05-14 14:17:14 -0300

    commit aula

--- Hotelaria/Hotel/urls.py
+++ Hotelaria/Hotel/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.Homepage, name='homepage')
+    path('', views.Homepage, name='homepage'),
+    path('login', views.Login, name='login'),
+    path('logout', views.Sair, name='logout'),
 ]
--- Hotelaria/Hotel/views.py
+++ Hotelaria/Hotel/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout, authenticate, login
+from django.contrib import messages
 from .models import *
 
 # Create your views here.
@@ -7,3 +9,24 @@ def Homepage(request):
     dados_home = homepage.objects.all()
     context['dados_home'] = dados_home
     return render(request, 'homepage.html', context)
+
+def Login(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('homepage')
+        else:
+            messages.error(request, 'Usuário ou senha inválidos.')
+
+    else:
+        return render(request, 'Login.html')
+
+def Sair(request):
+    logout(request)
+    return redirect ('homepage')
